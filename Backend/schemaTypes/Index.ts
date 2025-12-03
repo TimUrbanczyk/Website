b5162fa9fac82c88576e39e0c04c6ba099aeeac3
tirox67 <166658856+tirox67@users.noreply.github.com>
2025-12-04 00:55:18 +0100

    refactor: minor changes

--- Backend/schemaTypes/Index.ts
+++ Backend/schemaTypes/Index.ts
@@ -3,10 +3,11 @@ import { welcome } from './Welcome'
 import {socialList} from "./SocialList"
 import {navbar} from "./Navbar"
 import {text} from "./Text"
-
+import {cvList} from './CVList'
 import {imagePanel} from "./ImagePanel";
 
 export const schemaTypes = [
     cardList, welcome, socialList,
-    navbar, text, imagePanel
+    navbar, text, imagePanel,
+    cvList
 ]
